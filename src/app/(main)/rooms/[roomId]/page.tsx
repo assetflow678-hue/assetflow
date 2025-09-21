2d34f1f5dbaff6b6619bf871c2856132422e73c4
Flow Assets <assetflow678@gmail.com>
2025-09-21 07:11:51 +0000

    /rooms/4F6OIAW03oUhZb4ebW2v hãy chỉnh sửa lại skeleton loaders ở các tra

--- src/app/(main)/rooms/[roomId]/page.tsx
+++ src/app/(main)/rooms/[roomId]/page.tsx
@@ -109,20 +109,20 @@ const RoomDetailSkeleton = () => (
             <Table>
                 <TableHeader>
                     <TableRow>
-                        <TableHead><Skeleton className="h-5 w-24" /></TableHead>
-                        <TableHead><Skeleton className="h-5 w-48" /></TableHead>
-                        <TableHead><Skeleton className="h-5 w-24" /></TableHead>
-                        <TableHead><Skeleton className="h-5 w-24" /></TableHead>
-                        <TableHead><span className="sr-only">Actions</span></TableHead>
+                        <TableHead className="w-[30%]"><Skeleton className="h-5 w-full" /></TableHead>
+                        <TableHead className="w-[30%]"><Skeleton className="h-5 w-full" /></TableHead>
+                        <TableHead className="w-[20%]"><Skeleton className="h-5 w-full" /></TableHead>
+                        <TableHead className="w-[15%]"><Skeleton className="h-5 w-full" /></TableHead>
+                        <TableHead className="w-[5%]"><span className="sr-only">Actions</span></TableHead>
                     </TableRow>
                 </TableHeader>
                 <TableBody>
                     {Array.from({ length: 3 }).map((_, i) => (
                         <TableRow key={i}>
-                            <TableCell><Skeleton className="h-5 w-32" /></TableCell>
-                            <TableCell><Skeleton className="h-5 w-40" /></TableCell>
-                            <TableCell><Skeleton className="h-5 w-24" /></TableCell>
-                            <TableCell><Skeleton className="h-5 w-20" /></TableCell>
+                            <TableCell><Skeleton className="h-5 w-full" /></TableCell>
+                            <TableCell><Skeleton className="h-5 w-full" /></TableCell>
+                            <TableCell><Skeleton className="h-5 w-full" /></TableCell>
+                            <TableCell><Skeleton className="h-5 w-full" /></TableCell>
                             <TableCell><Skeleton className="h-8 w-8" /></TableCell>
                         </TableRow>
                     ))}
